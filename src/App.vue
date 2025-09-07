e59a038113d37ee274505ea446b3bd54b7d18ab3
unknown <razbydu@gmail.com>
2025-09-08 01:40:45 +1000

    some markup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Anonymous Questions App" />
+        <aside class="sidebar">
+            <div class="sidebar__inner">
+                <header class="sidebar__header">
+                    <h1 class="sidebar__title">
+                        ANONYMOUS QUESTIONS
+                    </h1>
+                </header>
+            </div>
+        </aside>
+
+        <main class="main">
+            <div class="container">
+                <v-field>What's your question?</v-field>
+            </div>
+        </main>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vField from './components/vField.vue';
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        vField
     }
 }
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+<style lang="scss">
+
+body {
+    --sidebar-width: 340px;
+    --sidebar-bg: #050505;
+
+    --container-width: 1050px;
+    --container-padding: 15px;
+
+
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    color: #000;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+
+    padding: 0;
+    margin: 0;
 }
+
+*,*::before,*::after {
+    box-sizing: border-box;
+}
+
+h1,h2,h3,h4,h5,h6 {
+    padding: 0;
+    margin: 0;
+}
+
+.sidebar {
+    width: var(--sidebar-width);
+
+    background: var(--sidebar-bg);
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+
+    &__header {
+        padding: 8px 8px 0;
+    }
+
+    &__title {
+        color: #fff;
+        font-size: 34px;
+        font-weight: 900;
+
+        line-height: .8;
+    }
+}
+
+.main {
+    padding-left: var(--sidebar-width);
+}
+
+.container {
+    max-width: var(--container-width);
+    padding: 0 var(--container-padding);
+    margin: 0 auto;
+    width: 100%;
+}
+
 </style>
---
+++ src/components/vField.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="field">
+        <label class="field__label"><slot></slot></label>
+        <textarea class="field__input"></textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "vField"
+}
+</script>
+
+<style lang="scss">
+    .field {
+        display: flex;
+        flex-direction: column;
+
+        &__label {
+            margin-bottom: 15px;
+
+            font-family: inherit;
+            font-weight: 900;
+            font-size: 30px;
+            color: #000;
+        }
+
+        &__input {
+            background: #F3F3F3;
+            border: 1px solid #E4E4E4;
+            
+            resize: none;
+
+            font-family: inherit;
+            font-size: 30px;
+            font-weight: 900;
+
+            height: auto;
+        }
+    }
+</style>
